bff9d7227fc98a5ab02253e2c38a763e3c97db81
Alexdelahaba95 <alejandromartinezdelahaba@gmail.com>
2019-06-17 14:17:27 +0200

    clase censo creada

--- API_REST/src/model/Censo.java
+++ API_REST/src/model/Censo.java
@@ -1,5 +1,35 @@
 package model;
 
 public class Censo {
-//hola
+	private String ciudad;
+	private String pais;
+	private Integer poblacion;
+	public Censo() {
+	}
+	
+	public Censo(String ciudad, String pais, Integer poblacion) {
+		super();
+		this.ciudad = ciudad;
+		this.pais = pais;
+		this.poblacion = poblacion;
+	}
+
+	public Integer getPoblacion() {
+		return poblacion;
+	}
+	public void setPoblacion(Integer poblacion) {
+		this.poblacion = poblacion;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
 }
