b841b70116c821db10fa42a4256cb7885739f25f
Alexdelahaba <alejandromartinezdelahaba@gmail.com>
2019-06-19 13:37:00 +0200

    ejemplo de excepcion creado

--- API_REST/src/controller/GestionCensoServlet.java
+++ API_REST/src/controller/GestionCensoServlet.java
@@ -14,16 +14,18 @@ import logic.GestionCenso;
 	public class GestionCensoServlet extends HttpServlet {
 		private static final long serialVersionUID = 1L;
 		
+		
 	    public GestionCensoServlet() {
 	        super();
 	    }
-
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			//response.getWriter().append(GestionCenso.getCenso(request).toString());
 		}
 
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			logic.Post.doPost(request);
+			//lanzamiento de errores al cliente
+			 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		}
 		
 		protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
