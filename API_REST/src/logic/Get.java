e1e9eecb2b9773276999ae9f66084f9d822f989f
Alexdelahaba <alejandromartinezdelahaba@gmail.com>
2019-06-20 17:49:52 +0200

    Planteamiento de la conversión a json en verbo get

--- API_REST/src/logic/Get.java
+++ API_REST/src/logic/Get.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.hibernate.Session;
 
+import com.google.gson.Gson;
+
 import controller.Error400;
 import controller.Error404;
 import dbm.HibernateUtil;
@@ -12,12 +14,20 @@ import model.Censo;
 public class Get {
 	public static String doGet(HttpServletRequest request) throws Error400, Error404{
 		Long indice = 1L;
-
+		String datosCenso = null;
 		Session session = HibernateUtil.getSession();
 		Censo c = session.get(Censo.class, indice);
 		session.close();
 
 		System.out.println("El valor de este censo vale: " + c);
-		return null;
+	
+		return datosCenso;
+	}
+@SuppressWarnings("unused")
+public static String ListJSON(String datosCenso) {		
+		
+		Session mySession = HibernateUtil.getSession();
+		return datosCenso;
+		
 	}
 }
