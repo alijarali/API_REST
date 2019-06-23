ba28b6e2857f493795e33c229910245ec64d5060
Leonor <Leonor@LEO-PC>
2019-06-23 16:04:18 +0200

    GET

--- API_REST/src/logic/Get.java
+++ API_REST/src/logic/Get.java
@@ -33,5 +33,52 @@ public class Get {
 	 List<Censo> lista = query.list();
 	 mySession.close();
 	}
+	 //Consulta a la base de datos
+	 String queryString = "";
+		
+		if(indice != null) {
+			//Consulto buscando por ID
+			queryString = "FROM Censo c WHERE C.indice =:indice";
+		}else {
+			//Consulta generica a la que se le iran añadiendo condiciones segun el caso
+			queryString = "FROM Censo C WHERE 1=1";
+			
+			if(ciudad != null)
+				queryString += " and C.ciudad =:ciudad";
+			if(pais != null)
+				queryString += " and C.pais =:pais";
+			if(poblacion != null)
+				queryString += " and C.poblacion =:poblacion";
+			if(from != null)
+				queryString += " and C.indice >=:from";
+			if(to != null)
+				queryString += " and C.indice <=:to";
+			if(sort != null)
+				queryString += " ORDER BY C."+sort+" ASC";
+		}
+
+		Session mySession = HibernateUtil.getSession();
+		query = mySession.createQuery(queryString);
+
+		if(id != null) {
+					query.setParameter("indice",indice);
+				}else {
+					if(tipo != null)
+						query.setParameter("ciudad", tipo);
+					if(nombre != null)
+						query.setParameter("pais", pais);
+					if(precio != null) 
+						query.setParameter("poblacion", poblacion);
+					if(from != null)
+						query.setParameter("from", from);
+					if(to != null)
+						query.setParameter("to", to);
+				}
+
+		List<Censo> lista = query.list();
+		mySession.close();
 
 }
+		
+		
+		
